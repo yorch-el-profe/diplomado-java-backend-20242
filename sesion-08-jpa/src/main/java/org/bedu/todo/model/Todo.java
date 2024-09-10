45a90a758f448b720229e2804927426b0f9d08ff
Jorge Ramón <jorge.ramon2128@gmail.com>
2024-09-09 20:11:52 -0600

    Agregando avance de JPA

--- sesion-08-jpa/src/main/java/org/bedu/todo/model/Todo.java
+++ sesion-08-jpa/src/main/java/org/bedu/todo/model/Todo.java
@@ -1,10 +1,29 @@
 package org.bedu.todo.model;
 
+import org.hibernate.annotations.ColumnDefault;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Todo {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
+
+    // Equivalente al NOT NULL
+    @Column(nullable = false)
     private String title;
+
+    @Column(nullable = false)
     private String description;
+
+    @Column
+    @ColumnDefault("false")
     private boolean completed;
 
     public int getId() {
