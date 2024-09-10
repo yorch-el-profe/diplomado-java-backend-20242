6917b9d4f17b295e9458b77322791a6770bbf0b6
Jorge Ramón <jorge.ramon2128@gmail.com>
2024-09-09 21:12:17 -0600

    Finalizando integración con JPA

--- sesion-08-jpa/src/main/java/org/bedu/todo/repository/TodoRepository.java
+++ sesion-08-jpa/src/main/java/org/bedu/todo/repository/TodoRepository.java
@@ -1,44 +1,34 @@
 package org.bedu.todo.repository;
 
-import java.util.ArrayList;
-import java.util.List;
 
 import org.bedu.todo.model.Todo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-// Repository es una clase que provee de método que manipulan datos
+import jakarta.transaction.Transactional;
+
+// Repository es una clase que provee de métodos que manipulan datos
 // y estos datos pueden estar almacenados en memoria (lista, arreglo, etc), en un archivo,
 // en una base de datos o en la nube.
 
 @Repository
-public class TodoRepository {
-    
-    private List<Todo> data = new ArrayList<>();
-    private int currentId = 0;
+public interface TodoRepository extends JpaRepository<Todo, Integer> {
 
-    public int size() {
-        return data.size();
-    }
+    // Métodos ya existentes en JPARepository
+    // findAll() -> Regresa todos los registros de la tabla
+    // findById(int id) -> Regresa el registro con el id indicado
+    // save(Todo todo) -> Crear un nuevo registro en la tabla / actualiza
+    // deleteById(int id) -> Elimina un registro por id
 
-    public List<Todo> findAll() {
-        return data;
-    }
 
-    public void save(Todo todo) {
-        todo.setId(++currentId);
-        todo.setCompleted(false);
-        
-        data.add(todo);
-    }
-
-    public void complete(int id) {
-        for (int i = 0; i < data.size(); i++) {
-            Todo current = data.get(i);
-
-            if (current.getId() == id) {
-                current.setCompleted(true);
-                break;
-            }
-        }
-    }
+    // JPQL -> Java Persistence Query Language
+    // SQL: UPDATE todos SET completed = TRUE WHERE id = :id
+    // JPQL: UPDATE Todo t SET t.completed = true WHERE t.id = :id
+    @Transactional
+    @Modifying
+    @Query("UPDATE Todo t SET t.completed = true WHERE t.id = :x")
+    void complete(@Param("x") int id);
 }
